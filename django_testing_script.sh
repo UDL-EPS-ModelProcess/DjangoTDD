ae38aef02a68aee1b70da09c254b1aeadee0ff36
aolite <aitorcorchero@gmail.com>
2017-04-09 13:37:06 +0200

    Add CicleCI compatibility and Test script

---
+++ django_testing_script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This file is generated due to the project structure. CicleCI detects automatically tests in the projetcs if they are
+# in the custom paths.
+
+# 1. Positionate into the correct foder
+
+cd mysite
+
+# Execute the commands for the tests
+
+python manage.py test
